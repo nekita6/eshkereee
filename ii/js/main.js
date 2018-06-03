e731ad0a174579885983897848f4a12924b3a58b
nekita6 <notatikin@yandex.ru>
2018-06-03 19:46:19 +0300

    рвыавыарвыашщ

--- ii/js/main.js
+++ ii/js/main.js
@@ -7,4 +7,8 @@ window.onload = function() {
 		// 	// document.querySelector('#after').style.clipPath = 'polygon(0 0, 0 100%, 100vw 100%, 100vw 0)';				
 		// } 
 	} 
+	document.querySelector('#menu-icon').onclick = function() {
+		this.classList.toggle('menu-open')
+		document.querySelector('footer').classList.toggle('open');
+	}
 }
