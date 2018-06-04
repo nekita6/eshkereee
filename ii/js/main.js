4ad92c435a1074e5ae43f3642c74cb806746bdab
nekita6 <notatikin@yandex.ru>
2018-06-04 19:19:47 +0300

    213123

--- ii/js/main.js
+++ ii/js/main.js
@@ -4,10 +4,16 @@ window.onload = function() {
 		if ( !preloader.classList.contains('done')) {
 			preloader.classList.add('done');
 		}
+		if ( !document.querySelector('#after').classList.contains('b-a')) {
+			document.querySelector('#after').classList.add('b-a');
+		}
 	}, 1000)
 	document.querySelector('#before').onmousemove = function (event) {
 		var x = event.offsetX;
 		document.querySelector('#after').style.width = x +'px';
+	} 
+	document.querySelector('#before').touchmove = function (event) {
+		console.log(event)
 	} 
 	document.querySelector('#menu-icon').onclick = function() {
 		this.classList.toggle('menu-open')
