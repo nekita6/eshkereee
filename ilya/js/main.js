f8248899190cdcc7ac20ac44eda3c3e186f41914
nekita6 <33751985+nekita6@users.noreply.github.com>
2018-10-14 19:45:39 +0300

    ilya

--- ilya/js/main.js
+++ ilya/js/main.js
@@ -2,18 +2,24 @@ var n=0;
 
 function changeWork() {
 	n++;
-	document.querySelector("#main-work h1").style.opacity = "0";
 	document.querySelector("#main-work").style.backgroundImage = "url('img/"+n+".jpg')";
+	document.querySelector("#main-work").style.backgroundPosition = "top center";
+	document.querySelector("#main-work").style.backgroundSize = "cover";
 	if (n==2) {
 		n=0;
 	}
 }
+
+setTimeout(function() {
+	document.querySelector("#main-work h1").style.opacity = "0";
+}, 2777);
+
 window.onload = function() {
 	var timeId = setInterval('changeWork()', 4000);
 	document.querySelector('#toggle-nav').onclick = function() {
-	document.querySelector('#toggle-nav').classList.toggle('menu-icon-open');
-	document.querySelector('nav').classList.toggle('open');
-}
+		document.querySelector('#toggle-nav').classList.toggle('toggle-nav-open');
+		document.querySelector('nav').classList.toggle('open');
+	}
 }
 
 
