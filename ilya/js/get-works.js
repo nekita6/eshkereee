66f3c84de356326ed0f3df6ddcca6070ea0cf034
nekita6 <33751985+nekita6@users.noreply.github.com>
2018-10-26 19:33:38 +0300

    no profile/faq

--- ilya/js/get-works.js
+++ ilya/js/get-works.js
@@ -1,31 +1,59 @@
 var string = '';
 var n = works.length;
-var i = 0;
-var j = 0;
+var i = --n;
+var j =i-9;
 
-i = --n;
-j = 0;
+
+//document.querySelector("#load-hiden").style.height = ;
 
 if (window.location.search != 0) {
 	var category = window.location.search.slice(1);
-		function getWorks() {
+	var categoryNav = document.getElementsByTagName('a');
+	for(var a=5; a < 11; a++){
+		if (categoryNav[a].search.slice(1) == category) {
+			categoryNav[a].classList.add('active');
+		}
+	}
+	function getWorks() {
+		var string = '';
 		for(i; i >= j; i--) {
 			if (works[i]['category'] == category) {
-				string += '<a href="card-work.html?'+ works[i]['id'] +'" class="work-preview"><figure><img src="'+ works[i]['preview'] +'" alt="work1"></figure></a>';
+				string += '<a href="card-work.html?'+ works[i]['id'] +'" class="work-preview"><figure><img src="'+ works[i]['preview'] +'" alt="'+ works[i]['category'] +'"></figure></a>';
 			}
 		}
-		document.querySelector('.work-list').innerHTML = string;
+		j=j-9;
+		if (j < 0) {
+			j = 0;
+		}
+		document.querySelector('.work-list').innerHTML += string;
+	}
+}
+else {
+	document.querySelector("#category-box a").classList.add('active');
+	function getWorks() {
+		var string = '';
+		for(i; i >= j; i--) {
+			string += '<a href="card-work.html?'+ works[i]['id'] +'" class="work-preview"><figure><img src="'+ works[i]['preview'] +'" alt="'+ works[i]['category'] +'"></figure></a>';
+		}
+		j=j-9;
+		if (j < 0) {
+			j = 0;
+		}
+		document.querySelector('.work-list').innerHTML += string;
 	}
 }
 
-function getWorks() {
-	for(i; i >= j; i--) {
-		string += '<a href="card-work.html?'+ works[i]['id'] +'" class="work-preview"><figure><img src="'+ works[i]['preview'] +'" alt="work1"></figure></a>';
-	}
-	document.querySelector('.work-list').innerHTML = string;
-}
 getWorks();
 
+
+
+function z() {
+	if (document.querySelector("#load-hiden").style.opacity == 0) {
+		document.querySelector("#load-hiden").style.display = 'none';
+	}
+}
 window.onload = function() {
 	document.querySelector("#load-hiden").style.opacity = 0;
+	setTimeout(z, 500);
+	//alert(document.documentElement.clientHeigth);
 }
