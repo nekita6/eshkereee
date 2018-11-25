8b3acc4a9f227aafa9396c6e5d4eb4a044e7e190
nekita6 <33751985+nekita6@users.noreply.github.com>
2018-11-25 22:41:22 +0200

    bibika

--- bibika/js/calc.js
+++ bibika/js/calc.js
@@ -1,67 +1,62 @@
-document.getElementById('longh').onkeypress = function(event) {
-	if (event.charCode < 46 || event.charCode > 57) return false
-	else if (event.charCode == 47) return false
-}
-
-document.getElementById('weighth').onkeypress = function(event) {
-	if (event.charCode < 46 || event.charCode > 57) return false
-	else if (event.charCode == 47) return false
-}
-
-document.getElementById('wraph').onkeypress = function(event) {
-	if (event.charCode < 46 || event.charCode > 57) return false
-	else if (event.charCode == 47) return false
-}
-
-document.getElementById('insuranceh').onkeypress = function(event) {
-	if (event.charCode < 46 || event.charCode > 57) return false
-	else if (event.charCode == 47) return false
-}
-
 document.onkeypress = function(event) {
-	console.log(event.code , event.charCode)
-	if (event.keyCode == 13) calc()
+	if (event.charCode < 46 || event.charCode > 57) return false
+	else if (event.charCode == 47) return false
 }
 
-function calc() {
+var p = 3.14;
 
-	var long = document.getElementById('longh').value;
-	var weight = document.getElementById('weighth').value;
-	var wrap = document.getElementById('wraph').value;
-	var insurance = document.getElementById('insuranceh').value;
-	var valid = /[^\d+,.]/g;
-
-	if (long =='' || weight =='')
-		alert('Немножко пусто! \nДлина пути и вес товара обязательны для заполнения')
-
-	else if (long == 0 || weight == 0)
-		alert('Длина пути и вес не могут быть равны 0')
-
-	else if (valid.test(long) == true || valid.test(weight) == true || valid.test(wrap) == true || valid.test(insurance) == true)
-		alert('Вводите только положительные числовые значение.')
-	
-	else if (long > 2000){
-		result = 'ОШИБКА! Максимальный путь перевозки 2000км';
-		alert(result)
-	}
-	
-	else if (weight > 40){
-		result = 'ОШИБКА! Максимальный вес груза 40кг';
-		alert(result)
-	}
-	
+function sShar() {
+	var R = document.getElementById('SharR').value;
+	if (R == '')
+		alert('Пусто!')
+	else if (R <= 0)
+		alert('Радиус должен быть больше нуля')
 	else {
-		result = long*21 + weight*10 + wrap*1 + insurance*1;
-		if (isNaN(result) == true)	
-			document.getElementById('out').innerHTML = 'Наверное, Вы ввели ... ;)';
-		else	
-			document.getElementById('out').innerHTML = result + ' &#8381;';
+		var S = 4*p*R*R;
+		document.getElementById('SharOut').innerHTML = S + ' усл. ед.';
 	}
-
 }
 
+function sKonus() {
+	var R = document.getElementById('KonusR').value;
+	var l = document.getElementById('Konusl').value;
+	if (R == '' || l == '')
+		alert('Пусто!')
+	else if (R <= 0 || l <= 0)
+		alert('Значения должны быть больше нуля')
+	else {
+		var S = p*R*R + p*R*l;
+		document.getElementById('KonusOut').innerHTML = S + ' усл. ед.';
+	}
+}
 
-/*/*10 – калькулятор для расчета стоимости доставки товара (задаются длина
-пути, вес товара или габариты, стоимость упаковки, страхование). Формулу
-расчета придумать самостоятельно;
+function sCilindr() {
+	var R = document.getElementById('CilindrR').value;
+	var h = document.getElementById('Cilindrh').value;
+	if (R == '' || h == '')
+		alert('Пусто!')
+	else if (R <= 0 || h <= 0)
+		alert('Значения должны быть больше нуля')
+	else {
+		var S = 2*p*R*h + 2*p*R*R;
+		document.getElementById('CilindrOut').innerHTML = S + ' усл. ед.';
+	}
+}
+
+function sUsKonus() {
+	var R1 = document.getElementById('UsKonusR1').value;
+	var R2 = document.getElementById('UsKonusR2').value;
+	var l = document.getElementById('UsKonusl').value;
+	if (R1 == '' || R2 == '' || l == '')
+		alert('Пусто!')
+	else if (R1 <= 0 || R2 == '' || l <= 0)
+		alert('Значения должны быть больше нуля')
+	else {
+		var S = p*R1*R1 + p*R1*l + p*R2*l + p*R2*R2;
+		document.getElementById('UsKonusOut').innerHTML = S + ' усл. ед.';
+	}
+}
+
+/*15  – калькулятор для расчета площади поверхности тел (шар, конус,
+цилиндр, усеченный конус);
 */
