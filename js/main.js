00b806348f102d6fcce4653238b7ceceed23bdad
nekita6 <33751985+nekita6@users.noreply.github.com>
2018-12-16 22:35:30 +0200

    -_-

--- js/main.js
+++ js/main.js
@@ -1,6 +1,4 @@
-document.querySelector('.menu-icon').onclick = function() {
-	document.querySelector('.menu-icon').classList.toggle('close');
-	document.querySelector('.menu').classList.toggle('is-open');
-	document.querySelector('.focus').classList.toggle('is-hidden');
-	document.querySelector('#lazar').classList.toggle('is-big');
+document.querySelector('#menu-icon').onclick = function() {
+	document.querySelector('#menu-icon').classList.toggle('close-icon');
+	document.querySelector('nav').classList.toggle('is-vision');
 }
