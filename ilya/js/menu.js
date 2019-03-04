cba43aa3eb2d135d10d14da71a1d5bb494f1f9b4
nekita6 <33751985+nekita6@users.noreply.github.com>
2019-03-04 12:48:27 +0300

    buuig

--- ilya/js/menu.js
+++ ilya/js/menu.js
@@ -2,6 +2,12 @@
 document.querySelector('#toggle-nav').onclick = function() {
 	document.querySelector('#toggle-nav').classList.toggle('toggle-nav-open');
 	document.querySelector('nav').classList.toggle('open');
+	if (document.querySelector('body').style.overflow != 'hidden') {
+		document.querySelector('body').style.overflow = 'hidden';
+	}
+	else {
+		document.querySelector('body').style.overflow = 'visible';
+	}
 }
 
 
