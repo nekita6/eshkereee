4c7f6b1f885e1914fa1f6a38d73c2eff3fd25354
nekita6 <33751985+nekita6@users.noreply.github.com>
2019-03-04 16:53:21 +0300

    04.03 DEADLINE

--- ilya/js/menu.js
+++ ilya/js/menu.js
@@ -2,18 +2,7 @@
 document.querySelector('#toggle-nav').onclick = function() {
 	document.querySelector('#toggle-nav').classList.toggle('toggle-nav-open');
 	document.querySelector('nav').classList.toggle('open');
-	if (document.querySelector('body').style.overflow != 'hidden') {
-	document.querySelector('body').style.overflow = 'hidden';
-	}
-	else {
-		document.querySelector('body').style.overflow = 'visible';
-	}
-	if (document.querySelector('header').style.position != 'fixed') {
-	document.querySelector('header').style.position = 'fixed';
-	}
-	else {
-		document.querySelector('body').style.position = 'relative';
-	}
+	document.querySelector('body').classList.toggle('toggle-body');
 }
 
 
