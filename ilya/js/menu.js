c9ccdfbe9e17c6dfa67f8b93edebf095e20d94ce
nekita6 <33751985+nekita6@users.noreply.github.com>
2019-03-04 15:28:03 +0300

    Update menu.js

--- ilya/js/menu.js
+++ ilya/js/menu.js
@@ -3,10 +3,16 @@ document.querySelector('#toggle-nav').onclick = function() {
 	document.querySelector('#toggle-nav').classList.toggle('toggle-nav-open');
 	document.querySelector('nav').classList.toggle('open');
 	if (document.querySelector('body').style.overflow != 'hidden') {
-		document.querySelector('body').style.overflow = 'hidden';
+	document.querySelector('body').style.overflow = 'hidden';
 	}
 	else {
 		document.querySelector('body').style.overflow = 'visible';
+	}
+	if (document.querySelector('header').style.position != 'fixed') {
+	document.querySelector('header').style.position = 'fixed';
+	}
+	else {
+		document.querySelector('body').style.position = 'relative';
 	}
 }
 
